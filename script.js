63c91bb55c6b0addaef33c4edb477b3accf48641
eric-merritt <ericm2009@gmail.com>
2023-08-09 03:08:22 -0600

    Changes to be committed: 	modified:   script.js     - By changing the value of i to the length of childNodes     (minus 3) in my div container for the 'book' div elements     I avoid books with repeating values that don't match the     array index.     -Added button to book divs which targets that element and     pops it from the library array as well as removes it from     the parent element     -Added function to change boolean value based on whether or     not the checkbox is checked for the 'read' value

--- script.js
+++ script.js
@@ -15,32 +15,6 @@ function Library() {
 
 let libBook = new Library();
 
-function domManip(){
-    for(let i = 0; i < libBook.book.length; i++){
-        const displayDiv = document.querySelector('div.display');
-        let bookDiv = document.createElement('div');
-        let title = document.createElement('h3');
-        let author = document.createElement('h3');
-        let pages = document.createElement('h3');
-        let read = document.createElement('h3');
-        let book_title = libBook.book[i].title;
-        let book_author = libBook.book[i].author;
-        let book_pages = libBook.book[i].pages;
-        let book_read = libBook.book[i].read;
-        title.textContent = 'Title: ' + book_title;
-        author.textContent = 'Author: ' + book_author;
-        pages.textContent = 'Pages: ' + book_pages;
-        read.textContent = 'Read: ' + book_read;
-        bookDiv.appendChild(title);
-        bookDiv.appendChild(author);
-        bookDiv.appendChild(pages);
-        bookDiv.appendChild(read);
-        bookDiv.className = 'bookDivs';
-        displayDiv.appendChild(bookDiv);
-        libBook.book.pop();
-    }
-}
-
 function showForm(){
     const form = document.querySelector('form.form');
     form.classList.remove('hidden');
@@ -52,22 +26,75 @@ addBtn.addEventListener('click',showForm);
 
 const submit = document.getElementById('submit');
 
+const form = document.querySelector('form.form');
+
+function checkRead() {
+    const checkbox = document.getElementById('read');
+    if (checkbox.checked == true){
+        checkbox.value = 'True';
+    } else {
+        checkbox.value = 'False';
+    }
+}
+
+
+
+
 submit.addEventListener('click', function getTarget(e) {
-    e.preventDefault()
+    e.preventDefault();
     let titleInput = document.getElementById('title').value;
     let authorInput = document.getElementById('author').value;
     let pagesInput = document.getElementById('pages').value;
     let readInput = document.getElementById('read').value;
     let newBook = new Book(titleInput, authorInput, pagesInput, readInput);
     libBook.addBook(newBook);
-    titleInput.value = '';
-    authorInput.value = '';
-    pagesInput.value = '';
-    readInput.value = '';
+    
     const form = document.querySelector('form.form');
     form.classList.add('hidden');
-    domManip();
+    createDivs();
 })
+
+const displayDiv = document.querySelector('div.display')        
+
+function createDivs(){
+for (let i = displayDiv.childNodes.length - 3; i < libBook.book.length; i++){
+        let bookDiv = document.createElement('div');
+            bookDiv.className = 'bookDivs';
+            bookDiv.value = `book[${i}]`;
+        
+
+        const removeBookBtn = document.createElement('button');
+            removeBookBtn.className = 'remove';
+            removeBookBtn.textContent = 'Remove';
+            removeBookBtn.value = i;
+
+            removeBookBtn.addEventListener('click', function(){
+                libBook.book.pop(this.value);
+                const targetDiv = this.parentElement;
+                displayDiv.removeChild(targetDiv);
+            })
+
+        let title = document.createElement('p');
+        let author = document.createElement('p');
+        let pages = document.createElement('p');
+        let read = document.createElement('p');
+        let book_title = libBook.book[i].title;
+        let book_author = libBook.book[i].author;
+        let book_pages = libBook.book[i].pages;
+        let book_read = libBook.book[i].read;
+        title.textContent = 'Title: ' + book_title;
+        author.textContent = 'Author: ' + book_author;
+        pages.textContent = 'Pages: ' + book_pages;
+        read.textContent = 'Read: ' + book_read;         
+        bookDiv.appendChild(title);
+        bookDiv.appendChild(author);
+        bookDiv.appendChild(pages);
+        bookDiv.appendChild(read);
+        bookDiv.appendChild(removeBookBtn);
+        
+        displayDiv.appendChild(bookDiv);
+}
+}
 
 
 
